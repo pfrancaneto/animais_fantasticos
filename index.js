e07ce81dcb42b4ef55f45e51ada3fa69ee4eb2b3
Pedro França <pfranca.neto@gmail.com>
2022-03-31 11:51:09 -0300

    ajustes - ativando as tabs nas imagens

---
+++ index.js
@@ -0,0 +1,24 @@
+function ativandoTabs() {
+  const tabMenu = document.querySelectorAll(".js-tabmenu li");
+  const tabContent = document.querySelectorAll(".js-tabcontent section");
+
+  if (tabMenu.length && tabContent.length) {
+    tabContent[0].classList.add("ativo");
+
+    function activeTabs(index) {
+      tabContent.forEach((item) => {
+        item.classList.remove("ativo");
+      });
+      tabContent[index].classList.add("ativo");
+    }
+
+    tabMenu.forEach((itemMenu, index) => {
+      itemMenu.addEventListener("click", function () {
+        activeTabs(index);
+      });
+    });
+  }
+}
+
+ativandoTabs();
+// ==================================================================
