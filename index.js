aafaf085a39de5a338ed24c27d896325d243bb11
Pedro França <pfranca.neto@gmail.com>
2022-03-31 23:42:05 -0300

    ajustes - ajustando o scroll suave / accordion

--- index.js
+++ index.js
@@ -21,4 +21,49 @@ function ativandoTabs() {
 }
 
 ativandoTabs();
-// ==================================================================
+// ==================================================== //
+
+function ativandoAccordion() {
+  const accordionList = document.querySelectorAll(".js-accordion dt");
+  accordionList[0].classList.add('ativo');
+  accordionList[0].nextElementSibling.classList.add('ativo');
+
+  function activeAccordion() {
+    this.classList.toggle("ativo");
+    this.nextElementSibling.classList.add("ativo");
+  }
+
+  accordionList.forEach((item) => {
+    item.addEventListener("click", activeAccordion);
+  });
+}
+ativandoAccordion();
+
+// =======================================================//
+
+// ativando scroolSuave
+function ativandoScrollSuave() {
+  const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+
+  function handleToScroll(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute("href");
+    const section = document.querySelector(href);
+
+    section.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+
+    // const topo = section.offsetTop;
+    // window.scrollTo({
+    //   top: topo,
+    //   behavior: 'smooth',
+    // });
+  }
+
+  linksInternos.forEach((link) => {
+    link.addEventListener("click", handleToScroll);
+  });
+}
+ativandoScrollSuave();
